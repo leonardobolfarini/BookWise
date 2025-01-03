3a0349cff6e3f9f9f8c7c9de245ede9fa6f08f11
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-03 14:52:10 -0300

    feat: Update home page styles to include grid layout and new sections for recent reviews and popular books

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -19,5 +19,45 @@ export const HomeHeader = styled('header', {
 })
 
 export const HomeContainer = styled('div', {
+    display: 'grid',
+    gridTemplateColumns: '1fr 324px',
+    gap: '64px',
+    margin: '0 6rem',
+})
 
+export const RecentReviews = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px',
+    
+    h2: {
+        fontSize: '$sm',
+        fontWeight: 'normal',
+    }
+})
+
+export const PopularBooksHeader = styled('div', {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '16px',
+
+    h2: {
+        fontSize: '$sm',
+        fontWeight: 'normal'
+    },
+
+    a: {
+        display: 'flex',
+        alignItems: 'center',
+        gap: '0.5rem',
+
+        fontSize: '$sm',
+        fontWeight: 'bold',
+        textDecoration: 'none',
+        color: '$purple-100',
+    }
+})
+
+export const PopularBooks = styled('div', {
 })
