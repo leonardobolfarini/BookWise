477ac036ccc468388163eb2ba166366a404cc832
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-03 14:51:52 -0300

    feat: Enhance home page with recent reviews and popular books sections

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,7 @@
-import { ChartLineUp } from "@phosphor-icons/react/dist/ssr";
-import { HomeContainer, HomeHeader } from "./styles";
+import { CaretRight, ChartLineUp } from "@phosphor-icons/react/dist/ssr";
+import { HomeContainer, HomeHeader, PopularBooks, PopularBooksHeader, RecentReviews } from "./styles";
+import { ReviewBox } from "./components/ReviewBox";
+import { BookCard} from "@/src/components/BookCard";
 
 export default function Home(){
     return (
@@ -9,6 +11,26 @@ export default function Home(){
                 <h1>Início</h1>
             </HomeHeader>
             <HomeContainer>
+                <RecentReviews>
+                    <h2>Avaliações mais recentes</h2>
+                    <ReviewBox />
+                    <ReviewBox />
+                    <ReviewBox />
+                </RecentReviews>
+
+                <PopularBooks>
+                    <PopularBooksHeader>
+                        <h2>Livros populares</h2>
+                        <a href="/search">
+                            Ver todos
+                            <CaretRight />
+                        </a>
+                    </PopularBooksHeader>
+                    <BookCard />
+                    <BookCard />
+                    <BookCard />
+                    <BookCard />
+                </PopularBooks>
             </HomeContainer>
         </>
     )
