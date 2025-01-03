7ace4a55a7cd126ededc37c0d7cebf04b5ff0906
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-03 17:26:12 -0300

    feat: Conditionally render Sidebar based on login page

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,14 @@
 import { Sidebar } from "@/src/components/Sidebar";
 import { AppLayoutStyles } from "./AppLayoutStyles";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  const isLoginPage = pathname === '/login'
+  
   return (
     <AppLayoutStyles>
-      <Sidebar />
+      {!isLoginPage ? <Sidebar /> : null}
       <main>{ children }</main>
     </AppLayoutStyles>
   )
