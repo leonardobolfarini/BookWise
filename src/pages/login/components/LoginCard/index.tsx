9f5156e14ae19ae83ff022751201421303682435
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-03 17:26:47 -0300

    feat: Add LoginCard component with styled container and image support

---
+++ src/pages/login/components/LoginCard/index.tsx
@@ -0,0 +1,23 @@
+import { IconProps } from "@phosphor-icons/react"
+import { LoginCardContainer, LoginCardContent } from "./styles"
+import Image, { StaticImageData } from "next/image"
+
+interface LoginCardProps extends React.HTMLProps<HTMLAnchorElement> {
+    children: React.ReactNode
+    ImageLogo: string | StaticImageData | React.ComponentType<IconProps>
+}
+
+export function LoginCard({ children, ImageLogo, ...rest }: LoginCardProps) {
+    return (
+        <LoginCardContainer {...rest}>
+            {typeof ImageLogo === 'string' || 'src' in ImageLogo ? (
+                <Image src={ImageLogo} alt='' width={32} height={32} />
+            ) : (
+                <ImageLogo size={32} />
+            )}
+            <LoginCardContent>
+                {children}
+            </LoginCardContent>
+        </LoginCardContainer>
+    )
+}
---
+++ src/pages/login/components/LoginCard/styles.ts
@@ -0,0 +1,38 @@
+import { styled } from "@/src/styles/stitches";
+
+export const LoginCardContainer = styled('a', {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '372px',
+    height: '72px',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+
+    backgroundColor: '$gray-600',
+    color: '$purple-100',
+    textDecoration: 'none',
+    borderRadius: '8px',
+    padding: '20px 24px',
+    gap: '20px',
+
+    cursor: 'pointer',
+    transition: 'background-color 0.2s',
+
+    '&:hover': {
+        backgroundColor: '$gray-500',
+        color: '$gradiant-horizontal',
+    },
+
+    '&:disabled': {
+        cursor: 'not-allowed',
+        backgroundColor: '$gray-500',
+        color: '$gray-200',
+    }
+})
+
+export const LoginCardContent = styled('div', {
+    fontWeight: 'bold',
+    fontSize: '$lg',
+    lineHeight: '$regular',
+    color: '$gray-200',
+})
