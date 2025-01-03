4965a9213beaeabf6cdd95669d5d217f96d2ffe8
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-03 14:49:44 -0300

    fix: Fixed the import after file name change

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "@/src/components/sidebar";
+import { Sidebar } from "@/src/components/Sidebar";
 import { AppLayoutStyles } from "./AppLayoutStyles";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
