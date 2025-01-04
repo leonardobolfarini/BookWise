8b13c75cce9156651925baa7461d0be82753a97a
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-04 02:43:44 -0300

    feat: Integrate NextAuth and React Query providers in the main app component for improved session management and data fetching

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,20 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 import Layout from "../layouts/AppLayout";
+import { SessionProvider } from "next-auth/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../lib/react-query";
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
