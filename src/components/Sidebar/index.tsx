d87ffd719eb8722679e631d9bc79e4eee69ad74c
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-04 02:41:56 -0300

    feat: Enhance Sidebar component to display user information and authentication state; integrate user data fetching and sign-out functionality

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,36 @@
 import Image from "next/image";
 import Logo from '@/src/assets/Logo.svg'
 import { SideBarContainer, SideBarHeader, SideBarFooter, SideBarNav, NavLink } from "./styles";
-import { Binoculars, ChartLineUp, SignIn } from "@phosphor-icons/react/dist/ssr";
+import { Binoculars, ChartLineUp, SignIn, SignOut } from "@phosphor-icons/react/dist/ssr";
 import { usePathname } from "next/navigation";
+import { api } from "@/src/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import { signOut } from "next-auth/react";
+
+interface User {
+    id: string
+    name: string | null
+    email: null | string
+    emailVerified: null | Date
+    image: string
+}
 
 export function Sidebar(){
     const currentPage = usePathname()
+
+    const { data: user } = useQuery<User>({
+        queryKey: ['user'],
+        queryFn: async () => {
+            const response = await api.get('/users')
+            return response.data.user
+        }
+    })
+    
+    const isAuthenticated = !!user
+
+    async function handleSignOut(){
+        await signOut()
+    }
     
     return (
         <SideBarContainer>
@@ -22,11 +47,21 @@ export function Sidebar(){
                     Explorar
                 </NavLink>
             </SideBarNav>
-            <SideBarFooter>
-                <a href="/login">
-                    Fazer login
-                    <SignIn width={24} height={24} />
-                </a>
+            <SideBarFooter authenticated={isAuthenticated}>
+                {!isAuthenticated ? (
+                    <a href="/login">
+                        Fazer login
+                        <SignIn width={24} height={24} />
+                    </a>
+                ) : (
+                    <a>
+                        <div>
+                            <Image src={user.image} alt="" width={32} height={32} />
+                        </div>
+                        {user?.name}
+                        <SignOut width={24} height={24} onClick={handleSignOut} />
+                    </a>
+                )}
             </SideBarFooter>
         </SideBarContainer>
     )
