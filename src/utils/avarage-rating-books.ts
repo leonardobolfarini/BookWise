2786073879c7a92480db236b28afa4dd723b5fcb
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-05 01:27:55 -0300

    feat: Implement average rating calculation for books and enhance book data with average ratings

---
+++ src/utils/avarage-rating-books.ts
@@ -0,0 +1,31 @@
+interface BookProps {
+    id: string;
+    Rating: {
+        rate: number;
+    }[];
+}
+
+export function averageRating(books: BookProps[]) {
+    return books.map((book) => {
+        const ratings = book.Rating.map((rating) => rating.rate)
+        if (ratings.length === 0) return { id: book.id, average: 0 }
+
+    const sum = ratings.reduce((currentValue, nextValue) => currentValue + nextValue, 0)
+
+    const average = Math.floor(sum / ratings.length)
+
+    return {
+        id: book.id,
+        average: average
+    }
+})}
+
+export function booksWithAverageRating(books: BookProps[], averageRating: { id: string; average: number }[]) {
+    return books.map((book) => {
+        const average = averageRating.find((average) => average.id === book.id)?.average
+        return {
+            ...book,
+            averageRating: average
+        }
+    })
+}
