013de14b87aff0f723453dd06a1f59842cd92cf0
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-05 01:27:29 -0300

    feat: Add API endpoint to retrieve book categories from the database

---
+++ src/pages/api/books/get-categories.api.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/src/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: 'Method not allowed'})
+    }
+
+    const categories = await prisma.category.findMany({
+        select: {
+            id: true,
+            name: true
+        }
+    })
+
+    return res.status(200).json(categories)
+}
