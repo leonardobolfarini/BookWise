9c7be09b0b0cfb3cc4f5dcc8de2d0c01f8f18030
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-05 01:29:16 -0300

    feat: Refactor BookCard component to accept dynamic book data props including title, author, average rating, and cover image

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,25 +1,30 @@
 import Image from "next/image";
-import Book from '@/src/assets/Book.svg'
 import { BooksCardContainer, BooksCardImage, BooksCardInfos } from "./styles";
 import { RatingStars } from "../RatingStars";
 
 interface BookCardProps {
+    title: string
+    author: string
+    averageRating: number
+    cover_url: string
     size?: 'sm' | 'md'
 }
 
-export function BookCard({ size = 'md' }: BookCardProps){
+export function BookCard(
+    { title, author, averageRating, cover_url, size = 'md' }
+    : BookCardProps){
     return (
         <BooksCardContainer size={size}>
             <BooksCardImage>
-                <Image src={Book} alt="" width={64} height={94} />
+                <Image src={cover_url} alt="" width={64} height={94} />
             </BooksCardImage>
             <BooksCardInfos>
                 <header>
-                    <h1>A revolução dos bichos</h1>
-                    <span>George Orwell</span>
+                    <h1>{title}</h1>
+                    <span>{author}</span>
                 </header>
                 <footer>
-                    <RatingStars ratingValue={4} />
+                    <RatingStars ratingValue={averageRating} />
                 </footer>
             </BooksCardInfos>
         </BooksCardContainer>
