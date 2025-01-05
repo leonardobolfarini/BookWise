45d1abb29cb84be14dbb8deec9632183cc78d975
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-05 01:26:40 -0300

    feat: Enhance AppLayoutStyles with responsive design for mobile screens

--- src/layouts/AppLayoutStyles.ts
+++ src/layouts/AppLayoutStyles.ts
@@ -4,4 +4,12 @@ export const AppLayoutStyles = styled('div', {
     display: 'grid',
     gridTemplateColumns: '323px 1fr',
     height: '100%',
+
+    '@media (max-width: 850px)': {
+        gridTemplateColumns: '1fr',
+        padding: '0 2rem',
+        maxWidth: '100%',
+        width: '100%',
+        boxSizing: 'border-box'
+    }
 })
