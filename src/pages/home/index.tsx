d949f5a1dfc5e16b1a4b48dbc5d6cd16adda5822
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-06 16:40:04 -0300

    feat: Integrate recent ratings and most rated books queries in Home component with dynamic rendering of ReviewBox and BookCard

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,11 @@
 import { CaretRight, ChartLineUp } from '@phosphor-icons/react/dist/ssr'
+import { useQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+
+import { BookProps } from '@/prisma/constants/books'
+import { BookCard } from '@/src/components/BookCard'
+import { api } from '@/src/lib/axios'
 
 import { ReviewBox } from './components/ReviewBox'
 import {
@@ -9,7 +16,56 @@ import {
   RecentReviews,
 } from './styles'
 
+export interface RatingPropsResponse {
+  id: string
+  rate: number
+  description: string
+  created_at: string
+  book: {
+    title: string
+    author: string
+    cover_url: string
+    summary: string
+  }
+  user: {
+    name: string
+    image: string
+  }
+}
+
+interface BookPropsResponse {
+  id: string
+  title: string
+  author: string
+  cover_url: string
+  summary: string
+  averageRating: number
+}
+
 export default function Home() {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  const { data: mostRatedBooks } = useQuery<BookPropsResponse[]>({
+    queryKey: ['mostRatedBooks'],
+    queryFn: async () => {
+      const response = await api.get('/books/most-rated-books')
+      return response.data
+    },
+  })
+
+  const { data: recentRatings } = useQuery<RatingPropsResponse[]>({
+    queryKey: ['recentRatings'],
+    queryFn: async () => {
+      const response = await api.get('/ratings/recent-ratings')
+      return response.data
+    },
+  })
+
+  function handleOpenDrawer(bookId: string) {
+    router.push(`/explore?popularBook=${bookId}`)
+  }
+
   return (
     <>
       <HomeHeader>
@@ -19,19 +75,39 @@ export default function Home() {
       <HomeContainer>
         <RecentReviews>
           <h2>Avaliações mais recentes</h2>
-          <ReviewBox />
-          <ReviewBox />
-          <ReviewBox />
+          {recentRatings?.map((rating) => {
+            return (
+              <ReviewBox
+                key={rating.id}
+                book={rating.book}
+                user={rating.user}
+                rating={rating.rate}
+                created_at={rating.created_at}
+              />
+            )
+          })}
         </RecentReviews>
 
         <PopularBooks>
           <PopularBooksHeader>
             <h2>Livros populares</h2>
-            <a href="/search">
+            <a href="/explore">
               Ver todos
               <CaretRight />
             </a>
           </PopularBooksHeader>
+          {mostRatedBooks?.map((book) => {
+            return (
+              <BookCard
+                key={book.id}
+                title={book.title}
+                author={book.author}
+                averageRating={book.averageRating}
+                cover_url={book.cover_url}
+                onClick={() => handleOpenDrawer(book.id)}
+              />
+            )
+          })}
         </PopularBooks>
       </HomeContainer>
     </>
