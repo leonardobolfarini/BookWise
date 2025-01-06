395b6b48bddd6ee415c5de8f4eea275f0610705f
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-06 04:13:53 -0300

    feat: Add API endpoint for submitting book reviews with rating and description

---
+++ src/pages/api/books/send-review.api.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/src/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' })
+    }
+
+    const { bookId, rate, description, userId } = req.body
+
+    await prisma.rating.create({
+        data: {
+            book_id: bookId,
+            user_id: userId,
+            rate,
+            description,
+        }
+    })
+
+    return res.status(201).end()
+}
