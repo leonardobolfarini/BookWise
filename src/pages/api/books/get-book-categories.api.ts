b6dd2e66383604c98af57e999b833ad885a5afeb
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-06 04:13:59 -0300

    feat: Add method validation to get-book-categories API endpoint to restrict to GET requests

--- src/pages/api/books/get-book-categories.api.ts
+++ src/pages/api/books/get-book-categories.api.ts
@@ -5,6 +5,10 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: 'Method not allowed' })
+    }
+
     const { categories_id } = req.query
 
     const categoriesIds = (categories_id as string).split(' ')
