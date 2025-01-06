b7db4c32481d34db8edc39a0a9e13a2742ec6fa9
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-05 22:35:07 -0300

    feat: Add ReviewCard component to display user reviews with rating, description, and timestamp

---
+++ src/pages/explore/components/ReviewCard/index.tsx
@@ -0,0 +1,50 @@
+import { RatingStars } from "@/src/components/RatingStars";
+import { ImageContainer, ReviewCardContainer, ReviewCardContent, ReviewCardHeader } from "./styles";
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import Image from "next/image";
+
+interface ReviewCardProps {
+    rate: number;
+    description: string;
+    created_at: string;
+    user: {
+        id: string;
+        name: string;
+        image: string;
+    };
+}
+
+export function ReviewCard({ rate, description, created_at, user }: ReviewCardProps) {
+    const createdAt = formatDistanceToNow(new Date(created_at), {
+        addSuffix: true,
+        locale: ptBR
+    })
+
+    return (
+        <ReviewCardContainer>
+            <ReviewCardHeader>
+                <header>
+                    <ImageContainer>
+                        <Image 
+                            src={user.image || '/default-avatar.png'}
+                            alt={user.name || 'Usuário'}
+                            width={40} 
+                            height={40} 
+                        />
+                    </ImageContainer>
+                    <div>
+                        <h3>{user.name}</h3>
+                        <span>{createdAt}</span>
+                    </div>
+                </header>
+                <footer>
+                    <RatingStars ratingValue={rate} />
+                </footer>
+            </ReviewCardHeader>
+            <ReviewCardContent>
+                <p>{description}</p>
+            </ReviewCardContent>
+        </ReviewCardContainer>
+    )
+}
