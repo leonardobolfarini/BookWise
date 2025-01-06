540712b7c2c013f785ec692b69f7c216dd29c7ad
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-05 22:35:42 -0300

    feat: Implement drawer component for book details and reviews, including comment form and enhanced user interaction

--- src/pages/explore/index.page.tsx
+++ src/pages/explore/index.page.tsx
@@ -1,5 +1,5 @@
-import { Binoculars } from "@phosphor-icons/react/dist/ssr";
-import { ExploreContainer, ExploreContent, ExploreFilters, ExploreHeader } from "./styles";
+import { Binoculars, X } from "@phosphor-icons/react/dist/ssr";
+import { DrawerComponent, DrawerContent, DrawerContentCloseButton, DrawerContentHeader, DrawerContentReviews, ExploreContainer, ExploreContent, ExploreFilters, ExploreHeader } from "./styles";
 import { SearchBar } from "@/src/components/SearchBar";
 import { TagBox } from "./components/TagBox/index";
 import { useQuery } from "@tanstack/react-query";
@@ -7,6 +7,13 @@ import { api } from "@/src/lib/axios";
 import { BookCard } from "@/src/components/BookCard";
 import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { BookInfosCard } from "./components/BookInfosCard";
+import { ReviewCard } from "./components/ReviewCard";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
 
 interface CategoryProps {
     id: string
@@ -21,15 +28,47 @@ interface BookProps {
     cover_url: string
     summary: string | null
     total_pages: number
+    categories: {
+        book_id: string;
+        category_id: string;
+    }[];
+    Rating: {
+        rate: number;
+        description: string;
+        created_at: string;
+        user: {
+            id: string;
+            name: string;
+            image: string;
+        };
+    }[];
 }
+
+const commentFormSchema = z.object({
+    description: z.string()
+        .min(3, { message: 'A descrição deve ter pelo menos 3 caracteres' })
+        .nullable(),
+    rate: z.number()
+        .min(1, { message: 'A avaliação deve ser pelo menos 1' })
+        .max(5, { message: 'A avaliação deve ser no máximo 5' })
+        .nullable(),
+})
+
+type CommentFormData = z.infer<typeof commentFormSchema>
 
 export default function Default() {
     const router = useRouter()
     const searchParams = useSearchParams()
 
+    const { data: session } = useSession()
+    const isAuthenticated = !!session
+
+    const [openBookId, setOpenBookId] = useState<string | null>(null)
+    const [openCommentForm, setOpenCommentForm] = useState(false)
+
     const categoriesForFilter = searchParams.get('categoriesForFilter') || ''
     const bookOrAuthorName = searchParams.get('bookOrAuthorName') || ''
-    
+
     const { data: categories } = useQuery<CategoryProps[]>({
         queryKey: ['categories'],
         queryFn: async () => {
@@ -49,6 +88,10 @@ export default function Default() {
             })
             return response.data
         }
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm<CommentFormData>({
+        resolver: zodResolver(commentFormSchema),
     })
 
     function handleAddFilter(filter: string) {
@@ -73,6 +116,14 @@ export default function Default() {
 
     function handleSearch(value: string) {
         router.push(`/explore?bookOrAuthorName=${value}`)
+    }
+
+    function handleOpenCloseDrawer(bookId: string | null) {
+        setOpenBookId(bookId)
+    }
+
+    function handleAddComment(bookId: string) {
+        setOpenCommentForm(true)
     }
 
     return (
@@ -109,7 +160,7 @@ export default function Default() {
             <ExploreContent>
                 {books?.map((book) => {
                     return (
-                        <div key={book.id}>
+                        <div key={book.id} onClick={() => handleOpenCloseDrawer(book.id)}>
                             <BookCard
                                 title={book.title}
                                 author={book.author}
@@ -117,6 +168,58 @@ export default function Default() {
                                 cover_url={book.cover_url}
                                 size="md"
                             />
+                            <DrawerComponent
+                                anchor="right"
+                                open={openBookId === book.id}
+                                onClose={() => handleOpenCloseDrawer(null)}
+                                slotProps={{
+                                    backdrop: {
+                                        sx: {
+                                            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                                        }
+                                    }
+                                }}
+                            >   
+                                <DrawerContent>
+                                    <DrawerContentHeader>
+                                        <DrawerContentCloseButton>
+                                            <button type="button" onClick={(e) => {
+                                                e.stopPropagation()
+                                                handleOpenCloseDrawer(null)
+                                            }}>
+                                                <X width={24} height={24} />
+                                            </button>
+                                        </DrawerContentCloseButton>
+                                        <BookInfosCard
+                                            id={book.id}
+                                            title={book.title}
+                                            author={book.author}
+                                            averageRating={book.averageRating}
+                                            categories={book.categories}
+                                            pages={book.total_pages}
+                                            cover_url={book.cover_url}
+                                            Rating={book.Rating}
+                                        />
+                                    </DrawerContentHeader>
+                                    <DrawerContentReviews>
+                                            <header>
+                                                <h2>Avaliações</h2>
+                                                <span>Avaliar</span>
+                                            </header>
+                                            {book.Rating.map((review) => {
+                                                return (
+                                                    <ReviewCard 
+                                                        key={review.user.id}
+                                                        user={review.user}
+                                                        description={review.description}
+                                                        created_at={review.created_at}
+                                                        rate={review.rate}
+                                                    />
+                                                )
+                                            })}
+                                    </DrawerContentReviews>     
+                                </DrawerContent>
+                            </DrawerComponent>
                         </div>
                     )
                 })}
