36394a772a5af028fc7e8a9ce119aea287948517
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-06 21:08:42 -0300

    feat: Implement server-side rendering for last user review in index page
    - Added getServerSideProps to fetch the last review of the logged-in user.
    - Integrated session management using next-auth to retrieve user session.
    - Utilized Prisma to query the last review, including related book and user data.
    - Ensured proper handling of cases where the user is not logged in.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,1 +1,43 @@
+import { GetServerSideProps } from 'next'
+import { getServerSession } from 'next-auth'
+
+import { prisma } from '../lib/prisma'
+import { authOptions } from './api/auth/[...nextauth].api'
+
 export { default } from './home'
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session?.user.id) {
+    return {
+      props: {
+        lastReview: null,
+      },
+    }
+  }
+
+  const lastReview = await prisma.rating.findFirst({
+    where: {
+      user_id: session.user.id,
+    },
+    include: {
+      book: true,
+      user: true,
+    },
+    orderBy: {
+      created_at: 'desc',
+    },
+  })
+
+  return {
+    props: {
+      lastReview: lastReview
+        ? {
+            ...JSON.parse(JSON.stringify(lastReview)),
+            created_at: lastReview.created_at.toISOString(),
+          }
+        : null,
+    },
+  }
+}
