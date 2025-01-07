60770127736e3b4cc2ee87b641437dfad8ec6388
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-06 21:12:03 -0300

    refactor: Added a prop to can reuse the component with same informations but with different style

--- src/pages/home/components/ReviewBox/index.tsx
+++ src/pages/home/components/ReviewBox/index.tsx
@@ -10,10 +10,11 @@ import {
   ReviewBoxContentContainer,
   ReviewBoxContentImage,
   ReviewBoxHeader,
+  ReviewBoxWhenLastReview,
 } from './styles'
 
 interface ReviewBoxProps {
-  user: {
+  user?: {
     name: string
     image: string
   }
@@ -25,9 +26,17 @@ interface ReviewBoxProps {
   }
   rating: number
   created_at: string
+
+  lastReview?: boolean
 }
 
-export function ReviewBox({ user, book, rating, created_at }: ReviewBoxProps) {
+export function ReviewBox({
+  user,
+  book,
+  rating,
+  created_at,
+  lastReview = false,
+}: ReviewBoxProps) {
   const createdAtFormatted = isToday(new Date(created_at))
     ? 'Hoje'
     : formatDistanceToNow(new Date(created_at), {
@@ -36,27 +45,38 @@ export function ReviewBox({ user, book, rating, created_at }: ReviewBoxProps) {
       })
 
   return (
-    <ReviewBoxContainer>
-      <ReviewBoxHeader>
-        <header>
-          <Image src={user.image} width={40} height={40} alt="" />
-          <div>
-            <p>{user.name}</p>
-            <span>{createdAtFormatted}</span>
-          </div>
-        </header>
-        <footer>
-          <RatingStars ratingValue={rating} />
-        </footer>
-      </ReviewBoxHeader>
+    <ReviewBoxContainer isLastReview={lastReview}>
+      {!lastReview ? (
+        <ReviewBoxHeader>
+          <header>
+            <Image src={user?.image || ''} width={40} height={40} alt="" />
+            <div>
+              <p>{user?.name}</p>
+              <span>{createdAtFormatted}</span>
+            </div>
+          </header>
+          <footer>
+            <RatingStars ratingValue={rating} />
+          </footer>
+        </ReviewBoxHeader>
+      ) : null}
+
       <ReviewBoxContentContainer>
         <ReviewBoxContentImage>
           <Image src={book.cover_url} width={108} height={152} alt="" />
         </ReviewBoxContentImage>
         <ReviewBoxContent>
           <header>
-            <h1>{book.title}</h1>
-            <span>{book.author}</span>
+            <div className={lastReview ? 'last-review-header' : ''}>
+              <div className="book-info">
+                <ReviewBoxWhenLastReview>
+                  {lastReview && <span>{createdAtFormatted}</span>}
+                  {lastReview && <RatingStars ratingValue={rating} />}
+                </ReviewBoxWhenLastReview>
+                <h1>{book.title}</h1>
+                <span>{book.author}</span>
+              </div>
+            </div>
           </header>
           <p>{book.summary}</p>
         </ReviewBoxContent>
