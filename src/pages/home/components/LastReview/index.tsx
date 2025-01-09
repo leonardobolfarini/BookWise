a16edd83236b6cc9419025739466315c6c8fb528
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-08 21:47:43 -0300

    refactor: Update LastReview component to include detailed user and book information, enhancing data structure and rendering logic

--- src/pages/home/components/LastReview/index.tsx
+++ src/pages/home/components/LastReview/index.tsx
@@ -1,23 +1,30 @@
 import { CaretRight } from '@phosphor-icons/react/dist/ssr'
-import { Rating } from '@prisma/client'
 import { useRouter } from 'next/router'
 
 import { ReviewBox } from '../../../../components/ReviewBox'
 import { LastReviewContainer } from './styles'
 
-interface RatingResponse extends Omit<Rating, 'created_at'> {
-  created_at: string
-  book: {
-    id: string
-    title: string
-    author: string
-    cover_url: string
-    summary: string
-  }
-}
-
 interface LastReviewProps {
-  lastReview: RatingResponse | null
+  lastReview: {
+    id: string
+    rate: number
+    description: string
+    created_at: string
+    book_id: string
+    user_id: string
+    book: {
+      id: string
+      title: string
+      author: string
+      cover_url: string
+      summary: string
+    }
+    user: {
+      id: string
+      name: string
+      image: string
+    }
+  }
 }
 
 export function LastReview({ lastReview }: LastReviewProps) {
@@ -28,7 +35,7 @@ export function LastReview({ lastReview }: LastReviewProps) {
     <LastReviewContainer>
       <div>
         <h2>Sua última leitura</h2>
-        <span onClick={() => router.push(`/users/${lastReview.user_id}`)}>
+        <span onClick={() => router.push(`/users/${lastReview.user.id}`)}>
           Ver todas <CaretRight width={16} height={16} />
         </span>
       </div>
