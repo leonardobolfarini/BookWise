ac19882d9d81dd67ede9aac0a29e2dc9e9f10b89
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-08 21:05:41 -0300

    feat: Create Avatar styles for customizable user profile images with responsive design options

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,52 @@
+import { styled } from '@/src/styles/stitches'
+
+export const AvatarContainer = styled('div', {
+  width: '74px',
+  height: '74px',
+
+  img: {
+    borderRadius: '999px',
+    objectFit: 'cover',
+
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+
+  variants: {
+    hasBorder: {
+      true: {
+        borderRadius: '999px',
+        background: '$gradiant-vertical',
+      },
+      false: {},
+    },
+    size: {
+      sm: {
+        width: '34px',
+        height: '34px',
+        img: {
+          width: '30px',
+          height: '30px',
+        },
+      },
+      md: {
+        width: '44px',
+        height: '44px',
+        img: {
+          width: '36px',
+          height: '36px',
+        },
+      },
+      lg: {
+        width: '74px',
+        height: '74px',
+        img: {
+          width: '70px',
+          height: '70px',
+        },
+      },
+    },
+  },
+})
