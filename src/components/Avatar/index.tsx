908d5654b70c92375d1829e32673849c82e4b1de
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-08 21:45:50 -0300

    feat: Enhance Avatar component with userId prop for profile navigation and update styles for improved interactivity across various components

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,13 +1,16 @@
 import Image, { ImageProps } from 'next/image'
+import { useRouter } from 'next/router'
 
 import { AvatarContainer } from './styles'
 
 interface AvatarProps extends ImageProps {
+  userId?: string
   size: 'sm' | 'md' | 'lg'
   hasBorder?: boolean
 }
 
 export function Avatar({
+  userId,
   width = 40,
   height = 40,
   hasBorder = false,
@@ -15,8 +18,20 @@ export function Avatar({
   alt = '',
   ...props
 }: AvatarProps) {
+  const router = useRouter()
+
+  function handleGoToUserProfile() {
+    if (!userId) return
+
+    router.push(`/users/${userId}`)
+  }
+
   return (
-    <AvatarContainer hasBorder={hasBorder} size={size}>
+    <AvatarContainer
+      hasBorder={hasBorder}
+      size={size}
+      onClick={handleGoToUserProfile}
+    >
       <Image {...props} alt={alt} width={width} height={height} />
     </AvatarContainer>
   )
